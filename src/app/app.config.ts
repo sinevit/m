4a92144469caf01c66a403ca2a6a47fb0c1a497a
Kristina <patrakovakris@gmail.com>
2024-12-06 18:47:53 +0300

    [feat] add taiga ui

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
+import {NG_EVENT_PLUGINS} from '@taiga-ui/event-plugins';
+import {provideAnimations} from '@angular/platform-browser/animations';
 import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
 import {provideRouter} from '@angular/router';
 
@@ -6,8 +8,10 @@ import {provideClientHydration, withEventReplay} from '@angular/platform-browser
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        provideAnimations(),
         provideZoneChangeDetection({eventCoalescing: true}),
         provideRouter(routes),
         provideClientHydration(withEventReplay()),
+        NG_EVENT_PLUGINS,
     ],
 };

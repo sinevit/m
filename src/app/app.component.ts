03053dea55bb092f43e62412fe10104f2a59c534
Kristina <patrakovakris@gmail.com>
2024-12-25 09:25:05 +0300

    [feat] add side-nav component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
 import {MainComponent} from './pages/main/main.component';
+import {TuiRoot} from '@taiga-ui/core';
+import {SideNavComponent} from './shared/components/side-nav/side-nav.component';
 
 @Component({
     standalone: true,
     selector: 'app-root',
-    imports: [RouterOutlet, MainComponent],
+    imports: [RouterOutlet, MainComponent, TuiRoot, SideNavComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
     title = 'meteo';
+
+    public isAuth: boolean = true;
 }
---
+++ src/app/shared/components/side-nav/side-nav.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+
+@Component({
+    standalone: true,
+    selector: 'app-side-nav',
+    imports: [],
+    templateUrl: './side-nav.component.html',
+    styleUrl: './side-nav.component.scss',
+})
+export class SideNavComponent {}
